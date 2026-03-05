62f3e9b1910420b3830c4d51f0567994cc108757
Jorge Gabriel <105565673+jorgegabrielsl@users.noreply.github.com>
2026-03-04 21:48:26 -0300

    Update performance_lojas.sql

--- performance_lojas.sql
+++ performance_lojas.sql
@@ -1,47 +1,4 @@
 
-with valor_total as (
-	select
-		C."name" as "categoria",
-		sum(P.amount) as "value_total"
-	
-	FROM public.payment P
-	inner join public.rental R on R.rental_id = P.rental_id
-	inner join public.inventory I on R.inventory_id = I.inventory_id
-	inner join Public.film F on F.film_id = I.film_id
-	inner join public.film_category FC on FC.film_id = F.film_id
-	inner join public.category C on C.category_id = FC.category_id
-    
-	C."name"
-
-)
-
-SELECT 
-	vl.categoria,
-	sum(vl.value_total) as "total",
-	round(
-		vl.value_total *100
-		/ sum(vl.value_total) over(),2
-	) as "%"
-
-FROM valor_total vl
-
-group by
-	vl.categoria,
-	vl.value_total
-
-order by 
-	vl.value_total DESC
-
-
-
-===================================================================================================
-
-
-
-
-
-===================================================================================================
-
 with fat_store as (
 
 Select 
@@ -69,6 +26,7 @@ from
 	fat_store
 where 
 	vl_total > 0
+
 
 
 
